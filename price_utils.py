626939810732ab7f60dcb4feda31827bf0a05527
Nenna <ninaokoye@outlook.com>
2019-07-12 00:37:09 +0100

    update price model with new stock

--- price_utils.py
+++ price_utils.py
@@ -3,26 +3,27 @@ from math import sin
 
 STOCKS = (
     'StockA',
+    'StockB',
 )
 
 
 def get_stock_prices():
     results = {}
-    for s in STOCKS:
-        results[s] = round(compute_price(), 3)
+    for i, s in enumerate(STOCKS):
+        results[s] = round(compute_price(i), 3)
     return results
 
 
-def compute_price():
+def compute_price(i):
     now = int(time.time()) - 1500000000
-    d = now / 600.0
+    d = now / 300.0
     return 4 + (
-        sin(2*d) / 2 +
-        sin(3*d) / 3 +
-        sin(5*d) / 4 +
-        sin(7*d) / 5 +
-        sin(11*d) / 6 +
-        sin(13*d) / 7 +
-        sin(17*d) / 8 +
-        sin(19*d) / 9
+        sin(2*(d+i)) / 2 +
+        sin(3*(d+i)) / 3 +
+        sin(5*(d+i)) / 4 +
+        sin(7*(d+i)) / 5 +
+        sin(11*(d+i)) / 6 +
+        sin(13*(d+i)) / 7 +
+        sin(17*(d+i)) / 8 +
+        sin(19*(d+i)) / 9
     )
